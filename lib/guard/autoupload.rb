1c1303389ba08f6d6d65d388be8230c4507bf44a
Jyrki Lilja <jyrkij.lilja@gmail.com>
2012-08-27 10:58:33 +0300

    Verbose and quiet output switches in options.

--- lib/guard/autoupload.rb
+++ lib/guard/autoupload.rb
@@ -29,6 +29,7 @@ module Guard
             @remote = options[:remote]
             @local = Dir.pwd
             @verbose = options[:verbose]
+            @quiet = options[:quiet] unless verbose?
 
             log "Initialized with watchers = #{watchers.inspect}"
             log "Initialized with options  = #{options.inspect}"
@@ -44,10 +45,11 @@ module Guard
                 attempts = 0
 
                 begin
-                    log "Upload #{local_file} => #{remote_file}"
+                    log "Upload #{local_file} => #{remote_file}" if verbose?
                     @session.upload!(local_file, remote_file)
-                rescue Exception => ex
-                    log "Exception on upload #{path}\n#{ex}"
+                    log "Uploaded #{path}" unless quiet?
+               rescue Exception => ex
+                    log "Exception on upload #{path}\n#{ex}" if verbose?
                     attempts += 1
                     remote_dir = File.dirname(remote_file)
                     recursively_create_dirs(remote_dir)
@@ -55,10 +57,9 @@ module Guard
                     log "Exceeded 3 attempts to upload #{path}"
                     throw :task_has_failed
                 end
-
-                log("Uploaded:\n#{paths.join("\n")}")
-                notify("Uploaded:\n#{paths.join("\n")}")
             end
+
+            notify("Uploaded:\n#{paths.join("\n")}")
         end
 
         def run_on_removals(paths)
@@ -66,14 +67,16 @@ module Guard
                 remote_file = File.join(@remote, path)
 
                 begin
-                    log "Delete #{remote_file}"
+                    log "Delete #{remote_file}" if verbose?
                     @session.remove!(remote_file)
                     @session.rmdir!(remote_file)
                 rescue Exception
                 end
 
-                notify("Deleted:\n#{paths.join("\n")}")
+                log "Deleted #{path}" unless quiet?
             end
+
+            notify("Deleted:\n#{paths.join("\n")}")
         end
 
         private
@@ -84,7 +87,7 @@ module Guard
                 new_dir = File.join(new_dir, dir)
 
                 begin
-                    log "Creating #{new_dir}"
+                    log "Creating #{new_dir}" unless quiet?
                     @session.mkdir!(new_dir)
                 rescue Exception
                     log "Cannot create directory #{new_dir}"
@@ -95,6 +98,10 @@ module Guard
 
         def verbose?
             @verbose || false
+        end
+
+        def quiet?
+            @quiet || false
         end
 
         def log(message)
