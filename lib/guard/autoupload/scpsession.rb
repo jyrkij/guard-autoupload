66e8a6a6d6a02c4980c0e335b4fed8419fe87bdc
Jyrki Lilja <jyrki.lilja@focus.fi>
2013-01-30 11:29:19 +0200

    Add SCP connection reset handling.

--- lib/guard/autoupload/scpsession.rb
+++ lib/guard/autoupload/scpsession.rb
@@ -6,23 +6,73 @@ class SCPSession
         @user = user
         @password = password.clone
         @caller = caller_ref
+        @retry_count = 0
+        @max_retries = 1
     end
 
     def ss
-        Thread.current[:simplessh] ||= Net::SSH::Simple.new({:user => @user, :password => @password})
+        Thread.current[:simplessh] ||= Net::SSH::Simple.new({:user => @user, :password => @password.clone})
     end
 
     def upload!(local, remote)
-        ss.scp_ul "#{@host}", "#{local}", "#{remote}"
+        begin
+            ss.scp_ul "#{@host}", "#{local}", "#{remote}"
+            # This shouldn't be run if we get an exception
+            @retry_count = 0
+        rescue Net::SSH::Simple::Error => e
+            case e.wrapped
+            when Errno::ECONNRESET
+                raise e if @retry_count >= @max_retries
+                @retry_count += 1
+                @caller.log "Failed uploading and will try again."
+                @caller.log "The reason was #{e}" unless @caller.quiet?
+                close
+                retry
+            else
+                raise e
+            end
+        end
     end
 
     def mkdir!(dir)
-        check_exists = ss.ssh "#{@host}", "ls -ld #{dir}"
-        ss.ssh "#{@host}", "mkdir #{dir}" unless check_exists.stdout
+        begin
+            check_exists = ss.ssh "#{@host}", "ls -ld #{dir}"
+            ss.ssh "#{@host}", "mkdir #{dir}" unless check_exists.stdout
+            # This shouldn't be run if we get an exception
+            @retry_count = 0
+        rescue Net::SSH::Simple::Error => e
+            case e.wrapped
+            when Errno::ECONNRESET
+                raise e if @retry_count >= @max_retries
+                @retry_count += 1
+                @caller.log "Failed making directory and will try again."
+                @caller.log "The reason was #{e}" unless @caller.quiet?
+                close
+                retry
+            else
+                raise e
+            end
+        end
     end
 
     def remove!(remote)
-        ss.ssh @host, "rm #{remote}"
+        begin
+            ss.ssh @host, "rm #{remote}"
+            # This shouldn't be run if we get an exception
+            @retry_count = 0
+        rescue Net::SSH::Simple::Error => e
+            case e.wrapped
+            when Errno::ECONNRESET
+                raise e if @retry_count >= @max_retries
+                @retry_count += 1
+                @caller.log "Failed removing file and will try again."
+                @caller.log "The reason was #{e}" unless @caller.quiet?
+                close
+                retry
+            else
+                raise e
+            end
+        end
     end
 
     def rmdir!(dir)
@@ -30,7 +80,25 @@ class SCPSession
     end
 
     def ls!(dir)
-        ss.ssh @host, "ls #{dir}"
+        begin
+            ss.ssh @host, "ls #{dir}"
+            # This shouldn't be run if we get an exception
+            @retry_count = 0
+        rescue Net::SSH::Simple::Error => e
+            case e.wrapped
+            when Errno::ECONNRESET
+                raise e if @retry_count >= @max_retries
+                puts @password
+                exit
+                @retry_count += 1
+                @caller.log "Failed listing directory contents and will try again."
+                @caller.log "The reason was #{e}" unless @caller.quiet?
+                close
+                retry
+            else
+                raise e
+            end
+        end
     end
 
     def close
