jyrkij/guard-autoupload

commit 3a8ed282ac9ab6b3152c04416850c1e88b6671be

Version that uploads but also fires the deletion for some reason

lib/guard/autoupload.rb
@@ -60,6 +60,10 @@ module Guard
                 path.sub!(/^#{@local_subpath}/, '')
                 remote_file = File.join(@remote, path)
 
+                if(File.directory?(local_file))
+                  next
+                end
+
                 attempts = 0
 
                 begin
@@ -85,22 +89,22 @@ module Guard
 
         def run_on_removals(paths)
             paths.each do |path|
-                path.sub!(/^#{@local_subpath}/, '')
-                remote_file = File.join(@remote, path)
-
-                begin
-                    UI.info("Delete #{remote_file}") if verbose?
-                    @session.remove!(remote_file)
-                rescue => ex
-                    UI.error("Exception on deleting #{path}\n#{ex.inspect.toutf8}")
-                    UI.error(ex.backtrace.join("\n")) if verbose?
-                end
-
-                UI.info("Deleted #{path}") unless quiet?
+              UI.info("trying to delete #{path}")
+            #    path.sub!(/^#{@local_subpath}/, '')
+            #    remote_file = File.join(@remote, path)
+            #
+            #    begin
+            #        UI.info("Delete #{remote_file}") if verbose?
+            #        @session.remove!(remote_file)
+            #    rescue => ex
+            #        UI.error("Exception on deleting #{path}\n#{ex.inspect.toutf8}")
+            #        UI.error(ex.backtrace.join("\n")) if verbose?
+            #    end
+            #
+            #    UI.info("Deleted #{path}") unless quiet?
             end
-
-            msg = "Deleted:\n#{paths.join("\n")}"
-            ::Guard::Notifier.notify msg, :title => "Deleted"
+            #
+            #::Guard::Notifier.notify "Deleted", :title => "Deleted"
         end
 
         def verbose?

lib/guard/autoupload/templates/Guardfile
@@ -16,8 +16,10 @@ opts = {
 }
 
 guard :autoupload, opts do
-  watch(/^((?!Guardfile$).)*$/)
-  ignore [%r{^.idea}, %r{^Guardfile}, %r{__jb_old__}, %r{__jb_bak__}]
-
-  # Matches every other file but Guardfile. This way we don't
+  watch(/^./)
+  ignore [%r{^.idea},
+          %r{^Guardfile}, #ignore the root guardfile
+          %r{__jb_old__}, %r{__jb_bak__}  # ruby idea ingores
+         ]
 end
+

commit 00b442578a36c2a220f27002fbce6ffc8b9b04f5

Make deletions work again (3a8ed282 broke these)

lib/guard/autoupload.rb
@@ -89,22 +89,21 @@ module Guard
 
         def run_on_removals(paths)
             paths.each do |path|
-              UI.info("trying to delete #{path}")
-            #    path.sub!(/^#{@local_subpath}/, '')
-            #    remote_file = File.join(@remote, path)
-            #
-            #    begin
-            #        UI.info("Delete #{remote_file}") if verbose?
-            #        @session.remove!(remote_file)
-            #    rescue => ex
-            #        UI.error("Exception on deleting #{path}\n#{ex.inspect.toutf8}")
-            #        UI.error(ex.backtrace.join("\n")) if verbose?
-            #    end
-            #
-            #    UI.info("Deleted #{path}") unless quiet?
+                path.sub!(/^#{@local_subpath}/, '')
+                remote_file = File.join(@remote, path)
+            
+                begin
+                    UI.info("Delete #{remote_file}") if verbose?
+                    @session.remove!(remote_file)
+                rescue => ex
+                    UI.error("Exception on deleting #{path}\n#{ex.inspect.toutf8}")
+                    UI.error(ex.backtrace.join("\n")) if verbose?
+                end
+            
+                UI.info("Deleted #{path}") unless quiet?
             end
-            #
-            #::Guard::Notifier.notify "Deleted", :title => "Deleted"
+            
+            ::Guard::Notifier.notify "Deleted", :title => "Deleted"
         end
 
         def verbose?